84938cf52603a179f8114492fabebf2fb0255429
NachoGranados <44333847+NachoGranados@users.noreply.github.com>
2020-03-01 11:24:50 -0600

    Diseño básico de la ventana del cliente.

---
+++ src/Sockets/ChatCliente.java
@@ -0,0 +1,43 @@
+package Sockets;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class ChatCliente {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ChatCliente window = new ChatCliente();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public ChatCliente() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
